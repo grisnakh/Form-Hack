067c6097eb411841a92b4578c7bdec6707baa9ba
Leonidas Christou <leon_christou@hotmail.com>
2016-09-12 13:28:46 +0300

    Add files via upload

---
+++ formhack.js
@@ -0,0 +1,11 @@
+document.body.style.border = "5px solid red";
+
+var arr = document.getElementsByTagName("input");
+for (var i = 0; i < arr.length; i++) {
+    if ( arr[i].type == 'hidden' ) {
+		
+		arr[i].setAttribute('type','text');
+	}
+		
+	
+}
