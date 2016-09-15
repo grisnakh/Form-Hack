281f825ee656a435fdbd244322e1c847f446e9d5
lchristou <l.christou@xe.gr>
2016-09-15 17:14:08 +0300

    Create LAF of widget

--- formhack.js
+++ formhack.js
@@ -1,11 +1,14 @@
-document.body.style.border = "5px solid red";
 
-
-var arr = document.getElementsByTagName("input");
-for (var i = 0; i < arr.length; i++) {
-    if ( arr[i].type == 'hidden' ) {
-		
-		arr[i].setAttribute('type','text');
+function ShowInputs() {
+	var arr = document.getElementsByTagName("input");
+	for (var i = 0; i < arr.length; i++) {
+		if ( arr[i].type == 'hidden' ) {
+			
+			arr[i].setAttribute('type','text');
+		}
+			
 	}
-		
+	return true;
 }
+
+document.getElementById("switch").onclick = function() {ShowInputs()};
