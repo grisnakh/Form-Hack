ec486797cdcd99b2d13cbf7d8ea2cbf7ab3d6027
lchristou <l.christou@xe.gr>
2016-09-16 16:55:28 +0300

    Workning Demo

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-function ShowInputs() {
-    var arr = document.getElementsByTagName("input");
-    for (var i = 0; i < arr.length; i++) {
-        if ( arr[i].type == 'hidden' ) {
-
-            arr[i].setAttribute('type','text');
-        }
-
-    }
-    // document.body.style.border = "5px solid red";
-
-}
-
 chrome.browserAction.onClicked.addListener(function(tab) {
 // No tabs or host permissions needed!
     console.log('Turning ' + tab.url + ' red!');
