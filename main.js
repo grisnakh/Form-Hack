968a5e815c00417c5a6ba39865e05d6230545c9c
lchristou <l.christou@xe.gr>
2016-09-19 13:31:36 +0300

    fixed untracked files

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 // No tabs or host permissions needed!
     console.log('Turning ' + tab.url + ' red!');
