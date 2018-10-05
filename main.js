b7ddb140ca89a8f910cb9593c84075d7752ef812
lchristou <l.christou@xe.gr>
2018-10-05 12:47:07 +0300

    display id of field

--- main.js
+++ main.js
@@ -2,42 +2,31 @@ function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
-function getImageData() {
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 100, 100);
- 
-  return ctx.getImageData(50, 50, 100, 100);
-}
-
-function updateIcon() {
-
-	if (!hidden_inputs) {
-		chrome.browserAction.setIcon({
-			96: "icons/icon-96.png"
-		})
-	} else {
-		chrome.browserAction.setIcon({
-			96: "icons/icon-96-enabled.png"	
-		})
-	} 
-
-}
-
 chrome.browserAction.onClicked.addListener(function(tab) {
 // No tabs or host permissions needed!
 
     chrome.tabs.executeScript({
-        code: 'function hasClass(a,b){return(" "+a.className+" ").indexOf(" "+b+" ")>-1}for(var arr=document.getElementsByTagName("input"),form_hacked="form_hacked",hidden_inputs=!0,i=0;i<arr.length;i++)hasClass(arr[i],form_hacked)?(updateIcon(),arr[i].setAttribute("type","hidden"),arr[i].className-="form_hacked",hidden_inputs=!0):"hidden"==arr[i].type&&(updateIcon(),arr[i].setAttribute("type","text"),arr[i].className+=" form_hacked",hidden_inputs=!1);'
-        // function: ShowInputs();
+        code: `
+            function hasClass(a,b){
+                return(" "+a.className+" ").indexOf(" "+b+" ")>-1
+            }
+
+            for(var arr=document.getElementsByTagName("input"),form_hacked="form_hacked",hidden_inputs=!0,i=0;i<arr.length;i++)
+            
+                hasClass(arr[i],form_hacked)?
+                    (arr[i].setAttribute("type","hidden"),
+                            elem = document.getElementById("explainText"),
+                            arr[i].parentNode.removeChild(elem),
+                            arr[i].classList.remove("form_hacked"),
+                            hidden_inputs=!0
+                    ):
+                "hidden"==arr[i].type&&(
+                    arr[i].setAttribute("type","text"),
+                        arr[i].classList.add("form_hacked"),            
+                        explainText = arr[i].getAttribute("name") ? arr[i].getAttribute("name") : (arr[i].id ? arr[i].id : "(not-set)"),
+                        arr[i].insertAdjacentHTML("beforebegin", "<div id='explainText' style='color: gray;display: block;z-index: 999; '>" + explainText + ":</div>"),
+                        hidden_inputs=!1
+                );
+        `
     });
-	
-	
 });
-
-
-/*chrome.browserAction.onClicked.addListener(() => {
-  chrome.browserAction.setIcon({imageData: getImageData()});
-});*/
